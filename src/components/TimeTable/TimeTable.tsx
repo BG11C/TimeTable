1f66bb3fc75dbd409b0e4fd3fcb964853d56a921
Julius Kirsch <Julius@skirsch.de>
2023-09-01 09:53:00 +0200

    feat: calculate time left until next hour

--- src/components/TimeTable/TimeTable.tsx
+++ src/components/TimeTable/TimeTable.tsx
@@ -1,7 +1,8 @@
 import { HourItem } from "../../models/HourItem";
+import { Subject } from "../../models/Subject";
 import { Weekday } from "../../models/Weekday";
 import styles from "./TimeTable.module.scss";
-
+import {useState, useEffect} from 'react';
 type Props = {
     accent1: string,
     accent2: string,
@@ -12,43 +13,93 @@ type Props = {
 }
 
 export default function TimeTable(props: Props) {
+    const [statusText, setStatusText] = useState("");
     let lastHighlighted = "";
 
     function isWeekend(day: number): boolean {
         return day > 5;
     }
 
-    function getCurrentHighlightIndex(date: Date): number{
-        const currentTime = parseInt(date.getHours().toString() + date.getMinutes().toString());
-        return props.hours.findIndex(x => parseInt(x.Start.replace(":", "")) <= currentTime &&  parseInt(x.End.replace(":", "")) > currentTime);
+    function getCurrentHighlightIndex(currentTime: number, currentWeekday: Weekday): number{
+        const index = props.hours.findIndex(x => parseInt(x.Start.replace(":", "")) <= currentTime && parseInt(x.End.replace(":", "")) > currentTime);
+        if(index >= currentWeekday.subjects.length)
+            return -1;
+        return index;
+    }
+
+    function detectDoubleLessons(currentHighlightIndex: number, currentDay: Weekday): number{
+        //Check whether there are hours left
+        if(currentHighlightIndex + 1 <  currentDay.subjects.length){
+            const nextHour = currentDay.subjects[currentHighlightIndex + 1];
+            const currentHour = currentDay.subjects[currentHighlightIndex];
+            if(nextHour.Name === currentHour.Name)  {
+                return 1 + detectDoubleLessons(currentHighlightIndex + 1, currentDay);
+            }
+        }
+        return 1;
+    }
+    
+    function subractTime(sub1: number, sub2: number): number{
+        const startTime = Math.floor(sub1 / 100) * 60 + (sub1 % 100); // Convert 1200 to 12:00
+        const endTime = Math.floor(sub2 / 100) * 60 + (sub2 % 100);   // Convert 500 to 5:00
+
+        // Calculate the time difference in minutes
+        return startTime - endTime;
+    }
+
+    function GetTimeToNextLesson(currentHighlightIndex: number, currentDay: Weekday, currentTime: number): string | null{
+        const hoursLeft = detectDoubleLessons(currentHighlightIndex, currentDay);
+        console.log(hoursLeft);
+        //only the current one left:
+        if(hoursLeft == 1) {
+            return subractTime(parseInt(props.hours[currentHighlightIndex].End.replace(":", "")), currentTime) + " Minuten"; 
+        }
+
+        return subractTime(parseInt(props.hours[currentHighlightIndex + hoursLeft - 1].End.replace(":", "")), currentTime) + " Minuten"; 
     }
 
     function highlightItem(){
         const date = new Date();
-        const dayOfWeek = date.getDay();
+        const dayOfWeek = date.getDay() - 1;
+        const currentTime = 1315;//parseInt(date.getHours().toString() + date.getMinutes().toString());
+        const currentWeekday = props.weekdays[dayOfWeek];
 
         if(lastHighlighted.length > 0)
             document.getElementById(lastHighlighted)?.classList.remove(styles.itemhighlighted);
 
+        //Weekend
         if(isWeekend(dayOfWeek)){
-            console.log("Wochenende");
             return;
         }
 
-        const item = getCurrentHighlightIndex(date);
-
-        if(item === -1)
+        const currentHighlightIndex = getCurrentHighlightIndex(currentTime, currentWeekday);
+        //Break or no more school
+        if(currentHighlightIndex === -1) {
+            if(currentTime > parseInt(props.hours[currentWeekday.subjects.length].End.replace(":", ""))){
+                //no more school
+                setStatusText("Schulschluss");
+                return;
+            }
+            //break:
+            setStatusText("Pause");
             return;
+        }
     
-        document.getElementById(`item${dayOfWeek - 1}_${item}`)?.classList.add(styles.itemhighlighted);
-        lastHighlighted = `item${dayOfWeek - 1}_${item}`;
-    }
+        const subject = currentWeekday.subjects[currentHighlightIndex];
+        const timeTillNextHour = GetTimeToNextLesson(currentHighlightIndex, currentWeekday, currentTime);
+        
+        setStatusText(`${subject.Name} bei ${subject.Teacher} in ${subject.Room} Noch ${timeTillNextHour}`);
 
-    highlightItem();
+        document.getElementById(`item${dayOfWeek}_${currentHighlightIndex}`)?.classList.add(styles.itemhighlighted);
+        lastHighlighted = `item${dayOfWeek}_${currentHighlightIndex}`;
+    }
+    useEffect(() => {
+        highlightItem();
+    })
     
     return (
         <div className={styles.timetable} style={{fontFamily: props.fontfamily}}>
-            <div className={styles.livestatustext}>Schulschluss</div>
+            <div className={styles.livestatustext}>{statusText}</div>
             <div className={styles.wrapper}>
                 <div className={styles.weekday}>
                     <div className={styles.houritem} style={{color: props.accent2}}>Stunden</div>
